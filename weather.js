49cf82e27aa711a2745bb91b69fe5c46c8208715
CaCaOKim <wmas2002kw@naver.com>
2021-03-05 19:33:17 +0900

    weather

--- weather.js
+++ weather.js
@@ -1,7 +1,18 @@
 const COORDS = 'coords';
 
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj))
+}
+
 function handleGeoSucces(poisition) {
-    console.log(poisition);
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    sabeCoords(coordsObj);
+
 }
 
 function handleGeoError() {
