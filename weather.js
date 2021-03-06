ca2f9ae4d0ccd66bdbe9348af9468ae2e6e6ee40
CaCaOKim <wmas2002kw@naver.com>
2021-03-07 01:02:03 +0900

    weather

--- weather.js
+++ weather.js
@@ -1,4 +1,9 @@
+const APU_KEY = "241051bf13976dd3ddf8b8d9f247255e"
 const COORDS = 'coords';
+
+function getWeather(lat, lng){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`)
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj))
@@ -12,7 +17,7 @@ function handleGeoSucces(poisition) {
         longitude
     };
     sabeCoords(coordsObj);
-
+    getWeather(latitude, longitude)
 }
 
 function handleGeoError() {
